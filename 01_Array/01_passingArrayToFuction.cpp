010145087abd74611fb0b0a1b41fa4ef24cc45f3
Sumit0925 <smtangural@gmail.com>
2024-10-10 21:12:38 +0530

    2nd Commit

--- 01_Array/01_passingArrayToFuction.cpp
+++ 01_Array/01_passingArrayToFuction.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+//! when is Array is passed in an Fuction It is passed with 
+//? "Pass By Refrence"
+
+void printArray (int arr[],int n) {  //* you can also use ( * arr ) instead of ( arr[] )
+    cout<<endl<<"Array in Function :";
+    for (int i = 0; i < n; i++) {
+        cout<<arr[i]<< " ";
+    }
+    
+}
+
+int main(){
 
     int arr[] = {1, 2, 3, 4, 5, 6};
     int n = sizeof(arr) / sizeof(int);
-    cout << n<<endl;
-    cout << "Hello world";
+    cout<<"Actual Array :";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    printArray(arr,n);
+    
 }
