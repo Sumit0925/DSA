253aa14021d9d928d202e7b0ac1bbd3361d7dfbf
Sumit0925 <smtangural@gmail.com>
2024-11-04 23:11:53 +0530

    12th Commit

---
+++ 03 Character Arrays/02_cin_get.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  char sentence[1000];
+
+  char temp = cin.get();
+
+  int len = 0;
+
+  // could have checked our len doesn't exceed the sentence array, additional
+  // check
+  while (temp != '\n') {
+    sentence[len++] = temp;
+    temp = cin.get();
+  }
+  sentence[len] = '\0';  // termination the last element with null character
+                         // otherwise it will give us Garbage Value
+
+  cout << "Length " << len << endl;
+
+  cout << sentence << endl;
+}
